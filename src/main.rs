475bf196c7712e36b1455f6dc16315d1d735e269
Lasne Olivier <lasneolivier@gmail.com>
2025-05-30 13:25:20 +0200

    added nom and defined point

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 use color_eyre::Result;
+use nom::{bytes::take_while_m_n, combinator::map_res, IResult};
+use nom::character::complete::digit1;
 use std::fs;
+use std::num::ParseIntError;
+
+#[derive(Debug, PartialEq)]
+pub struct Point {
+    pub x: u8,
+    pub y: u8,
+}
+
+fn is_digit(input: &str) -> Result<u8, ParseIntError> {
+    u8::from_str_radix(input, 10)
+}
+
+fn parse_int(input: &str) -> IResult<&str, u8> {
+    map_res(
+        take_while_m_n(1, 1, digit1),
+        is_digit
+    ).parse(input)
+}
+
 
 fn main() -> Result<()> {
     color_eyre::install()?;
